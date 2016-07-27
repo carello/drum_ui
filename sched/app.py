f1b9a13c8d855bf363cadbbed00f941d8dda4497
cpuskarz <cpuskarz@cisco.com>
2016-07-26 19:25:39 -0500

    baseline phase 3

--- sched/app.py
+++ sched/app.py
@@ -8,23 +8,21 @@ __author__ = 'cpuskarz'
 
 app = Flask(__name__)
 
-#APPSERVER = "http://127.0.0.1:5002"
+APPSERVER = "http://127.0.0.1:5002"
 
 @app.route('/')
 def drummer_list():
     u = APPSERVER + "/options"
-
     page = requests.get(u)
     options = page.json()
-    #drummer_list = options["drummers"]
     drummer_list = options["options"]
-
     return render_template('home.html', drummer_list=drummer_list)
     
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
 
+'''
 @app.route("/results")
 def results():
     # Check for submitted vote
@@ -35,8 +33,28 @@ def results():
     page = u.read()
     resp = json.loads(page)
     return resp
+'''
+
+@app.route("/results", methods=["GET"])
+def res():
+    pick = request.args.get('drummer')
+    pick = pick.replace(" ", "")
+    pick = pick.lower()
+    u = requests.post(APPSERVER + "/" + pick)
+    page = u.content
+    resp = json.loads(page)
+    return resp
+
+'''
+@app.route("/pit", methods=["GET"])
+def pit():
+    u = requests.post(APPSERVER + "/chetcarello")
+    print u.content
+    return u.content
+'''
+
 
 
 if __name__ == '__main__':
-    APPSERVER = os.getenv('app_server')
+    #APPSERVER = os.getenv('app_server')
     app.run(debug=True, host='0.0.0.0')
