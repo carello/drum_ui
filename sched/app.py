a782e4d32aee4582f71afb787d2f7aafa4875bf3
cpuskarz <cpuskarz@cisco.com>
2016-07-27 11:37:59 -0500

    completed POST for all tiers  On branch phase3  Changes to be committed: 	new file:   .gitignore 	modified:   sched/app.py

--- sched/app.py
+++ sched/app.py
@@ -22,38 +22,6 @@ def drummer_list():
 def about():
     return render_template('about.html', title="About")
 
-'''
-@app.route("/results")
-def results():
-    # Check for submitted vote
-    seldrummer = request.args.get('drummer')
-    seldrummer = seldrummer.replace(" ", "")
-    seldrummer = seldrummer.lower()
-    u = urllib.urlopen(APPSERVER + "/"+ seldrummer)
-    page = u.read()
-    resp = json.loads(page)
-    return resp
-'''
-# following works - dont delete
-'''
-@app.route("/results", methods=["GET"])
-def res():
-    pick = request.args.get('drummer')
-    pick = pick.replace(" ", "")
-    pick = pick.lower()
-    u = requests.post(APPSERVER + "/" + pick)
-    page = u.content
-    resp = json.loads(page)
-    return resp
-'''
-
-'''
-@app.route("/pit", methods=["GET"])
-def pit():
-    u = requests.post(APPSERVER + "/chetcarello")
-    print u.content
-    return u.content
-'''
 
 # trying to send payload to app server with POST
 @app.route("/results", methods=["GET"])
@@ -63,9 +31,7 @@ def respick():
     drumman = drumman.lower()
     u = requests.post(APPSERVER + "/mess", data=drumman)
     page = u.content
-    resp = json.loads(page)
-    return resp
-
+    return page
 
 if __name__ == '__main__':
     #APPSERVER = os.getenv('app_server')
